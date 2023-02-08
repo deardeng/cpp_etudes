5c4bd40f81085013dd69c97d420ef8f368280bbc
wangwuchen <wuchenwang@gaugene.com>
2023-02-08 15:46:27 +0800

    day01 endpoint

---
+++ day01-endpoint/day01-endpoint/day01-endpoint.cpp
@@ -0,0 +1,12 @@
+﻿   
+#include <iostream>   
+#include "endpoint.h"
+using namespace std;
+using namespace boost;
+
+int main()
+{
+	client_end_point();
+	server_end_point();
+	return 0;
+}
---
+++ day01-endpoint/day01-endpoint/endpoint.h
@@ -0,0 +1,8 @@
+#ifndef  __END_POINT_H__
+#define __END_POINT_H__
+#include <boost/asio.hpp>
+#include <iostream>
+
+extern  int  client_end_point();
+extern  int  server_end_point();
+#endif
