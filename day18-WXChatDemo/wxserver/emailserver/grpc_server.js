868af20b3e19f70388cccf8bbf5e08f3d1327c86
wangwuchen <wuchenwang@gaugene.com>
2023-06-20 19:50:26 +0800

    grpc server 处理验证码

--- day18-WXChatDemo/wxserver/emailserver/grpc_server.js
+++ day18-WXChatDemo/wxserver/emailserver/grpc_server.js
@@ -1,15 +1,18 @@
 const grpc = require('@grpc/grpc-js')
-const hello_proto = require('./proto')
+const message_proto = require('./proto')
 
 let cnt = 1
 
-function sayHello(call, callback) {
-    callback(null, { message: `[${cnt++}] echo: ` + call.request.message })
+function GetVarifyCode(call, callback) {
+    callback(null, { email: `[${cnt++}] echo: ` + call.request.email,
+                code:"1024",
+                error:0
+             });
 }
 
 function main() {
     var server = new grpc.Server()
-    server.addService(hello_proto.Greeter.service, { sayHello: sayHello })
+    server.addService(message_proto.VarifyService.service, { GetVarifyCode: GetVarifyCode })
     server.bindAsync('0.0.0.0:50051', grpc.ServerCredentials.createInsecure(), () => {
         server.start()
         console.log('grpc server started')        
