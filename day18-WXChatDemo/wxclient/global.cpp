1b91dae41bc14ff02ab0741324539d10c78baa24
wangwuchen <wuchenwang@gaugene.com>
2023-06-26 14:37:27 +0800

    添加beast websocket。

--- day18-WXChatDemo/wxclient/global.cpp
+++
@@ -1,6 +0,0 @@
-#include <QString>
-QString g_app_path = "";
-QString g_config_path ="";
-QString g_webhost ="";
-QString g_webport ="";
-QString g_weburl_prefix="";
--- day18-WXChatDemo/wxserver/webserver/webserver/const.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-enum ErrorCodes {
-	Success = 0,
-	Error_Json = 1001,  //Json½âÎö´íÎó
-	RPCFailed = 1002,  //RPCÇëÇó´íÎó
-};
-
