57be556b87978dfe42c34992393872bea86ccf3b
wangwuchen <wuchenwang@gaugene.com>
2023-07-31 17:15:42 +0800

    添加线程并发代码

---
+++ concurrent/day01-thread/day01-thread.cpp
@@ -0,0 +1,18 @@
+﻿// day01-thread.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <thread>
+
+void thead_work1(std::string str) {
+
+}
+
+int main()
+{
+    std::string hellostr = "hello world!";
+    std::thread t1(thead_work1, hellostr);
+    t1.join();
+}
+
+
