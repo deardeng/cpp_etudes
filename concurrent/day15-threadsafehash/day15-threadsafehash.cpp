56b555fb4133cc6558bddfc3664118185ec6a65f
wangwuchen <wuchenwang@gaugene.com>
2023-11-23 18:39:45 +0800

    提交线程安全的查找结构

---
+++ concurrent/day15-threadsafehash/day15-threadsafehash.cpp
@@ -0,0 +1,23 @@
+﻿// day15-threadsafehash.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include "threadsafetable.h"
+
+void TestThreadSafeHash() {
+
+    threadsafe_lookup_table table;
+    std::thread t1([]() {
+        
+        });
+
+    std::thread t2([]() {
+        
+        });
+}
+
+int main()
+{
+    
+}
+
