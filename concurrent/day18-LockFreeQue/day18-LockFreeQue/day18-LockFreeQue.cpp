cf974018038144a6e2d4df4c9bdd19e083022528
secondtonone1 <secondtonone1@163.com>
2024-01-07 18:51:00 +0800

    添加无锁队列

---
+++ concurrent/day18-LockFreeQue/day18-LockFreeQue/day18-LockFreeQue.cpp
@@ -0,0 +1,71 @@
+﻿// day18-LockFreeQue.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include "singlepushpop.h"
+#include <thread>
+#include "lockfreeque.h"
+#include <cassert>
+
+void TestSinglePopPush() {
+    SinglePopPush<int>  que;
+    std::thread t1([&]() {
+        for (int i = 0; i < 10000; i++) {
+            que.push(i);
+            std::cout << "push data is " << i << std::endl;
+            std::this_thread::sleep_for(std::chrono::milliseconds(10));
+            }
+        });
+
+    std::thread t2([&]() {
+        for (int i = 0; i < 10000; i++) {
+           auto p =  que.pop();
+           if (p == nullptr) {
+               std::this_thread::sleep_for(std::chrono::milliseconds(10));
+               continue;
+           }
+           
+           std::cout << "pop data is " << *p << std::endl;
+        }
+      });
+
+    t1.join();
+    t2.join();
+}
+
+void TestLockFreeQue() {
+    lock_free_queue<int>  que;
+    std::thread t1([&]() {
+        for (int i = 0; i < 10000; i++) {
+            que.push(i);
+            std::cout << "push data is " << i << std::endl;
+            std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        }
+        });
+
+    std::thread t2([&]() {
+        for (int i = 0; i < 10000;) {
+            auto p = que.pop();
+            if (p == nullptr) {
+                std::this_thread::sleep_for(std::chrono::milliseconds(10));
+                continue;
+            }
+            i++;
+            std::cout << "pop data is " << *p << std::endl;
+        }
+        });
+
+    t1.join();
+    t2.join();
+
+    assert(que.destruct_count == 9999);
+}
+
+int main()
+{
+    //TestSinglePopPush();
+    TestLockFreeQue();
+    std::cout << "Hello World!\n";
+}
+
+
